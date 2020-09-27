a15cfd48adc6daa30917311e8440ee947004777b
Rafael Kallis <rk@rafaelkallis.com>
2020-09-27 15:15:54 +0200

    test: improved check_prefix test

--- test/node.cpp
+++ test/node.cpp
@@ -30,19 +30,22 @@ TEST_SUITE("node") {
   TEST_CASE("check_prefix") {
     leaf_node<int> node(nullptr);
     string key = "000100001";
+    int key_len = key.length() + 1; // +1 for \0
     string prefix = "0000";
+    int prefix_len = prefix.length() + 1; // +1 for \0
 
     node.prefix_ = (char *) prefix.c_str();
-    node.prefix_len_ = prefix.length();
+    node.prefix_len_ = prefix_len;
 
-    CHECK_EQ(3, node.check_prefix(key.c_str() + 0, key.length() - 0));
-    CHECK_EQ(2, node.check_prefix(key.c_str() + 1, key.length() - 1));
-    CHECK_EQ(1, node.check_prefix(key.c_str() + 2, key.length() - 2));
-    CHECK_EQ(0, node.check_prefix(key.c_str() + 3, key.length() - 3));
-    CHECK_EQ(4, node.check_prefix(key.c_str() + 4, key.length() - 4));
-    CHECK_EQ(3, node.check_prefix(key.c_str() + 5, key.length() - 5));
-    CHECK_EQ(2, node.check_prefix(key.c_str() + 6, key.length() - 6));
-    CHECK_EQ(1, node.check_prefix(key.c_str() + 7, key.length() - 7));
-    CHECK_EQ(0, node.check_prefix(key.c_str() + 8, key.length() - 8));
+    CHECK_EQ(3, node.check_prefix(key.c_str() + 0, key_len - 0));
+    CHECK_EQ(2, node.check_prefix(key.c_str() + 1, key_len - 1));
+    CHECK_EQ(1, node.check_prefix(key.c_str() + 2, key_len - 2));
+    CHECK_EQ(0, node.check_prefix(key.c_str() + 3, key_len - 3));
+    CHECK_EQ(4, node.check_prefix(key.c_str() + 4, key_len - 4));
+    CHECK_EQ(3, node.check_prefix(key.c_str() + 5, key_len - 5));
+    CHECK_EQ(2, node.check_prefix(key.c_str() + 6, key_len - 6));
+    CHECK_EQ(1, node.check_prefix(key.c_str() + 7, key_len - 7));
+    CHECK_EQ(0, node.check_prefix(key.c_str() + 8, key_len - 8));
+    CHECK_EQ(0, node.check_prefix(key.c_str() + 9, key_len - 9));
   }
 }
